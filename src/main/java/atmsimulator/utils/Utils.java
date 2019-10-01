ae6e2642c4ef3f7cc4a4c939ce920600f1517590
linhdt <linh626688@gmail.com>
2019-10-01 13:57:00 +0700

    refactor import

--- src/main/java/atmsimulator/utils/Utils.java
+++ src/main/java/atmsimulator/utils/Utils.java
@@ -14,45 +14,10 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static atmsimulator.Constant.CSV_SEPARATOR;
 import static atmsimulator.Constant.REGEX_MATCH_NUMBER;
 
 public class Utils {
     private static final Random generator = new Random();
-
-    public static void writeTransactionLog(Transaction transaction) {
-        {
-            try {
-                StringBuffer firstLine = new StringBuffer();
-                firstLine.append("Account Number,Time,Amount,Type,Ref\n");
-                File tempFile = new File("./transaction.csv");
-                boolean isExistsFile = tempFile.exists();
-                PrintWriter bw = new PrintWriter(new BufferedWriter(new FileWriter("transaction.csv", true)));
-                // header line
-                if (!isExistsFile) {
-                    bw.write(firstLine.toString());
-                }
-                // log
-                StringBuffer oneLine = new StringBuffer();
-                oneLine.append(transaction.getAccountNumber());
-                oneLine.append(CSV_SEPARATOR);
-                oneLine.append(transaction.getTime());
-                oneLine.append(CSV_SEPARATOR);
-                oneLine.append(transaction.getAmount());
-                oneLine.append(CSV_SEPARATOR);
-                oneLine.append(transaction.getType());
-                oneLine.append(CSV_SEPARATOR);
-                oneLine.append(transaction.getRef());
-                oneLine.append('\n');
-                bw.write(oneLine.toString());
-
-                bw.flush();
-                bw.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
 
     public static List<Transaction> getTransactionFromCSV() {
         String fileName = "./transaction.csv";
@@ -81,7 +46,7 @@ public class Utils {
     }
 
     public static List<Account> importAccountFromCSV() {
-        String fileName = "./input_account.csv";
+        String fileName = ".src/main/resources/input_account.csv";
         List<String> list = new ArrayList<>();
         List<Account> result = new ArrayList<>();
 
@@ -103,7 +68,7 @@ public class Utils {
             account.setAccountNumber(infoUser[3]);
             result.add(account);
         });
-        return  result;
+        return result;
     }
 
     public static DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm a");
